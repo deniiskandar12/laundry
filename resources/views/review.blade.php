a62910cef2e793d8c6874b8c6923cc01df4c74ab
Garongan <93327526+Garongan@users.noreply.github.com>
2021-11-14 22:29:47 +0700

    Update review.blade.php

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -7,21 +7,40 @@
 
         <div class="card-body text-dark mt-5 mx-5 rounded" style="background: #E5E5E5">
             <h5 class="card-title">Primary card title</h5>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
 
         <div class="card-body text-dark mt-5 mx-5 rounded" style="background: #E5E5E5">
             <h5 class="card-title">Primary card title</h5>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
-
         <div class="card-body text-dark mt-5 mx-5 rounded" style="background: #E5E5E5">
             <h5 class="card-title">Primary card title</h5>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
 
         <div class="card-body text-dark mb-5 mt-5 mx-5 rounded" style="background: #E5E5E5">
             <h5 class="card-title">Primary card title</h5>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
     </div>    
