294181f6efd5b41d4fb6f9a7e17e3b01762f810f
Garongan <93327526+Garongan@users.noreply.github.com>
2021-11-14 22:34:56 +0700

    Update user.blade.php

--- resources/views/layout/user.blade.php
+++ resources/views/layout/user.blade.php
@@ -8,14 +8,20 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
     
     <title>Mama Laundry | {{ $title }} </title>
 
     <style>
         .card-cover {
-        background-repeat: no-repeat;
         background-position: center center;
         background-size: cover;
+        }
+        .checked {
+            color: orange;
+        }
+        .modal-content {
+            background: #E5E5E5;
         }
     </style>
 </head>
@@ -26,34 +32,8 @@
         <nav class="navbar navbar-light bg-light">
             <!-- brand -->
             <a class="navbar-brand fw-bold" href="/" style="margin-left: 10%" >Mama Laundry</a>
-            <!-- toggler -->
-            <button class="navbar-toggler" style="margin-right: 10%" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <!-- offcanvas menu -->
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link " href="/user/dashboard">Dashboard</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/user/review-kami">Pesan</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/user/riwayat-pesananmu">Checkout</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/user/riwayat-pesananmu">Review Kami</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
+            <!-- username nama -->
+            <p class="navbar-brand nav justify-content-end" style="margin-right: 10%">Username</p>
         </nav>
     </div>
     <!-- content -->
@@ -64,7 +44,25 @@
         </div>
 
 
-
+        <script>
+            $(document).ready(function() {
+                $('#rateMe1').mdbRate();
+            });
+            function alertSuccessReview() {
+              alert("Review Berhasil Ditambahkan");
+            }
+            function alertSuccessPesan() {
+              alert("Pesanan Berhasil Ditambahkan");
+            }
+            function alertSuccessBayar() {
+              alert("Pengiriman Bukti Pembayaran Sukses");
+            }
+            function alertSuccessUpload() {
+              alert("Bukti Berhasil Diunggah");
+            }
+        </script>
+        
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </script>
     </body>
 </html>
